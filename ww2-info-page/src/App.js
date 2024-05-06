09e449a40c2a544e3a1dbcbf224f9e95a159b2f4
SametAltintas <samet236236@gmail.com>
2024-05-06 23:34:01 +0300

    header-finished

--- ww2-info-page/src/App.js
+++ ww2-info-page/src/App.js
@@ -1,5 +1,6 @@
 import Header from "./components/Header";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import "./style/background.css"
 
 
 function App() {
--- ww2-info-page/src/components/Header.jsx
+++ ww2-info-page/src/components/Header.jsx
@@ -1,8 +1,9 @@
 import img from "../image/images.jpeg";
 const Header = () => {
   return (
-    <div>
+    <div className=" mt-4">
         <img src={img} alt=""/>
+        <h1 style={{fontFamily:'UnifrakturMaguntia, cursive'}} className=" mt-4 text-white">WW2 Tanks</h1>
     </div>
   )
 }
