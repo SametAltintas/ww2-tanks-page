c7f00fee529f965880b2cffa04a0267a52ae77f9
SametAltintas <samet236236@gmail.com>
2024-05-07 00:07:08 +0300

    header-finished

--- ww2-info-page/src/components/Header.jsx
+++ ww2-info-page/src/components/Header.jsx
@@ -1,8 +1,8 @@
-import img from "../image/images.jpeg";
+import img from "../image/tank-icon.jpg";
 const Header = () => {
   return (
     <div className=" mt-4">
-        <img src={img} alt=""/>
+        <img src={img} alt="" className="border border-black rounded-circle"/>
         <h1 style={{fontFamily:'UnifrakturMaguntia, cursive'}} className=" mt-4 text-white">WW2 Tanks</h1>
     </div>
   )
